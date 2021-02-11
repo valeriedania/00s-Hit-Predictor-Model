3bf9f9346a92ff6dabb5c7ea9c63d37682e8b514
Valerie Dania <73624288+valeriedania@users.noreply.github.com>
2021-02-11 17:38:30 -0500

    Group categorical data and quantitative data

--- column_datasource.py
+++ column_datasource.py
@@ -6,25 +6,22 @@ from scipy.stats.kde import gaussian_kde
 from bokeh.io import output_file, show
 from bokeh.models import ColumnDataSource, FixedTicker, PrintfTickFormatter, HoverTool
 from bokeh.plotting import figure
+from bokeh.models.tools import HoverTool
 from bokeh.sampledata.perceptions import probly
 
 #Save our figure with .html extension
 ouput_file = ('hitpredictormodel.html')
 
 #Insert the absolute path of csv into method
-data = pd.read_csv('/Users/Valerie/dataset-of-00s.csv')
+df = pd.read_csv('/Users/Valerie/dataset-of-00s.csv')
 
-#Use only a subset of columns in csv file and store in df
-df = pd.DataFrame(data, columns = ['Track', 'Artist', 'Danceability', 'Energy', 'Speechiness', 'Liveness', 'Valence', 'Target'])
+#Group track per hit factors
+grouped = df.groupby('Track')[ 'artist', 'danceability', 'energy', 'speechiness', 'liveness', 'valence', 'target'].sum() * 100
+
 
 #Random sample in df
 sample = df.sample(50)
 
-#Store sample in source
+#Pass data into columndatasource and store sample in source
 source = ColumnDataSource(sample)
 
-#Calculate percentage
-hit_factors = ['Danceabilitiy', 'Energy', 'Speechiness', 'Liveness', 'Valence', 'Target']
-df['percent'] = (df[hit_factors]/ df[hit_factors].sum()) * 100
-
-print(df)
